890406aacf4501bfc81df399bdf032aa41b15bb6
0RAVEN0 <43756842+0raven0@users.noreply.github.com>
2020-05-04 14:01:10 +0200

    add zoom function

--- src/main/map_gui/Controller.java
+++ src/main/map_gui/Controller.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +15,9 @@ public class Controller implements Initializable {
     @FXML
     private ImageView Imagemap;
 
+    @FXML
+    private AnchorPane mapWindow;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -20,5 +25,28 @@ public class Controller implements Initializable {
         Image image = new Image("file:data/blueprint1.png",1000,1000,true,true);
 
         Imagemap.setImage(image);
+    }
+
+    @FXML
+    private void Zoom(ScrollEvent event){
+        event.consume();
+
+        double mapZoom = 0.0;
+
+        if (event.getDeltaY() > 0){
+            mapZoom = 1.1;
+        }
+        else{
+            mapZoom = 0.9;
+        }
+
+        //to not go behind window
+        if (mapZoom * Imagemap.getScaleX() > 1.15){
+            return;
+        }
+
+        Imagemap.setScaleX(mapZoom * Imagemap.getScaleX());
+        Imagemap.setScaleY(mapZoom * Imagemap.getScaleY());
+
     }
 }
