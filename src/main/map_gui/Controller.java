35808f5c2f75934558b773f2af729b60082c0242
0RAVEN0 <43756842+0raven0@users.noreply.github.com>
2020-05-04 13:29:17 +0200

    load image from file

--- src/main/map_gui/Controller.java
+++ src/main/map_gui/Controller.java
@@ -1,4 +1,24 @@
 package main.map_gui;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ImageView Imagemap;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //Image image = new Image("file:src/main/map_gui/blueprint1.png");
+        Image image = new Image("file:data/blueprint1.png",1000,1000,true,true);
+
+        Imagemap.setImage(image);
+    }
 }
