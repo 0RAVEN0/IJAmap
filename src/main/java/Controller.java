159951c25559e5713e8b1de07e0ed60a7825f3bc
0RAVEN0 <43756842+0raven0@users.noreply.github.com>
2020-05-05 22:42:23 +0200

    add load map from pc

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,30 +1,36 @@
 package main.java;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    private int mapLink;
+    private int lineLink;
+
     @FXML
     private ImageView Imagemap;
 
-    @FXML
-    private AnchorPane mapWindow;
-
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //Image image = new Image("file:src/main/map_gui/blueprint1.png");
-        Image image = new Image("file:data/blueprint1.png",1000,1000,true,true);
-
-        Imagemap.setImage(image);
     }
 
     @FXML
@@ -40,13 +46,32 @@ public class Controller implements Initializable {
             mapZoom = 0.9;
         }
 
-        //to not go behind window
-        if (mapZoom * Imagemap.getScaleX() > 1.15){
-            return;
-        }
-
         Imagemap.setScaleX(mapZoom * Imagemap.getScaleX());
         Imagemap.setScaleY(mapZoom * Imagemap.getScaleY());
+
+    }
+
+    public void mapClick(ActionEvent actionEvent) {
+        JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        fc.setDialogTitle("Open your map: ");
+
+        mapLink = fc.showOpenDialog(null);
+        File selectedFile = fc.getSelectedFile();
+
+        /*if (mapLink == JFileChooser.APPROVE_OPTION) {
+            selectedFile = fc.getSelectedFile();
+            System.out.println(selectedFile.getAbsolutePath());
+        }*/
+
+        Image image = new Image(selectedFile.toURI().toString(),1000,1000,true,true);
+        Imagemap.setImage(image);
+    }
+
+    public void lineClick(ActionEvent actionEvent) {
+        JFileChooser fc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        fc.setDialogTitle("Open your line: ");
+
+        lineLink = fc.showOpenDialog(null);
 
     }
 }
