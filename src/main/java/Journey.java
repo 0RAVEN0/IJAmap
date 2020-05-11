0fae48b55c01a851e5bf6bef7ff0abb7a282dc2c
Leenoha <leenoha@gmail.com>
2020-05-11 23:09:08 +0200

    prepare new class for journeys

---
+++ src/main/java/Journey.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public class Journey {
+    private String id;
+    private List<Stop> stops;
+    private List<LocalTime> starts;
+    private List<Schedule> sequence;
+}
