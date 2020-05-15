0325b7383e07cef5d7330e20bc74da1d75fb8f4e
Leenoha <leenoha@gmail.com>
2020-05-15 17:32:34 +0200

    add method for displaying circle

--- src/main/java/ShapeCircle.java
+++ src/main/java/ShapeCircle.java
@@ -1,6 +1,8 @@
 package main.java;
 
 import javafx.scene.Cursor;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
@@ -14,5 +16,17 @@ public class ShapeCircle {
         circle.setId(id);
         circle.setCursor(Cursor.HAND);
         return circle;
+    }
+
+    public static void display(Circle busCircle, String circleId, List<Circle> circles, Pane mapWindow) {
+        for (Circle circle : circles) {
+            if (circle.getId().equals(circleId)) {
+                mapWindow.getChildren().remove(circle);
+            }
+        }
+        busCircle.setFill(Color.LAVENDER);
+        busCircle.setStroke(Color.PURPLE);
+        circles.add(busCircle);
+        mapWindow.getChildren().add(busCircle);
     }
 }
